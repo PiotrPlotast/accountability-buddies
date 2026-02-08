01a16a7d0966b620e6ce6d1187c9b4d88860422e
Piotr Plotast <piotrplotast2006@gmail.com>
2026-02-08 17:10:52 +0100

    Wraps dashboard view in React StrictMode
    Enables React StrictMode to help identify potential problems
    by activating additional checks and warnings during development.
    Aims to improve code quality and future-proof the component.

--- app/(protected)/(tabs)/index.tsx
+++ app/(protected)/(tabs)/index.tsx
@@ -2,21 +2,24 @@ import "../../../global.css";
 import { View } from "react-native";
 import Dashboard from "@/app/components/dashboard/Dashboard";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { StrictMode } from "react";
 
 export default function Page() {
   const insets = useSafeAreaInsets();
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        paddingTop: insets.top,
-        paddingBottom: insets.bottom,
-      }}
-    >
-      <Dashboard />
-    </View>
+    <StrictMode>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+        }}
+      >
+        <Dashboard />
+      </View>
+    </StrictMode>
   );
 }
