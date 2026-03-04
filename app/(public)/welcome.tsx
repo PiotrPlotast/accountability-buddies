aa849103402eac77761171b05b22bdd2e2add0b2
Piotr Plotast <piotrplotast2006@gmail.com>
2026-03-04 20:07:04 +0100

    added basic styles for buttons

--- app/(public)/welcome.tsx
+++ app/(public)/welcome.tsx
@@ -1,4 +1,4 @@
-import { Button } from "react-native";
+import { Text, Pressable } from "react-native";
 
 import { router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -6,10 +6,28 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function Page() {
   return (
     <SafeAreaView
-      style={{ flex: 1, alignItems: "center", justifyContent: "flex-end" }}
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 16,
+      }}
     >
-      <Button title="Sign Up" onPress={() => router.push("/sign-up")} />
-      <Button title="Sign In" onPress={() => router.push("/sign-in")} />
+      <Text className="text-4xl text-center">
+        Welcome to{"\n"}Accountability Buddies
+      </Text>
+      <Pressable
+        className="bg-gray-300 w-full p-4 rounded-full justify-center items-center"
+        onPress={() => router.push("/sign-up")}
+      >
+        <Text>Go to sign up</Text>
+      </Pressable>
+      <Pressable
+        className="bg-gray-300 w-full p-4 rounded-full justify-center items-center"
+        onPress={() => router.push("/sign-in")}
+      >
+        <Text>Go to sign in</Text>
+      </Pressable>
     </SafeAreaView>
   );
 }
